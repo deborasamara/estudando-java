03b017f074d69b599714a040ee08425c2c28a4fc
deborasamara <deborasamara.ro@gmail.com>
2026-04-20 16:35:01 -0300

    Add Usumake class extending Ninja with attributes and methods

--- video-canal-fiasco/Usumake.java
+++ video-canal-fiasco/Usumake.java
@@ -1,3 +1,6 @@
-public class Usumake {
-    
+public class Usumake extends Ninja {
+    boolean temBiju;
+    public void shacraInfinito(){
+        System.out.println("VC é usumake, então vc tem shacra infinito, parabéns!");
+    }
 }
