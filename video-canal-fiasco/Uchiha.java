2a98478d51c80803cc08320f4d831a64970d81d4
deborasamara <deborasamara.ro@gmail.com>
2026-04-19 22:23:17 -0300

    Add Uchiha class extending Ninja with Sharingan functionality

---
+++ video-canal-fiasco/Uchiha.java
@@ -0,0 +1,6 @@
+public class Uchiha extends Ninja { // Herança
+    String nivelDoSharingan = "dois";
+    public void sharinganAtivado(){
+        System.out.println("sharin ativadooooooo");
+    }
+}
