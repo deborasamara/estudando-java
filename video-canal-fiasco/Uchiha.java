60d56ab78b8b49cf2ac54db9218bd83ccf0f9d04
deborasamara <deborasamara.ro@gmail.com>
2026-04-20 16:35:09 -0300

    Refactor Uchiha class to initialize nivelDoSharingan and enhance ataqueBase methods

--- video-canal-fiasco/Uchiha.java
+++ video-canal-fiasco/Uchiha.java
@@ -1,6 +1,22 @@
 public class Uchiha extends Ninja { // Herança
-    String nivelDoSharingan = "dois";
+    String nivelDoSharingan;
+
     public void sharinganAtivado(){
         System.out.println("sharin ativadooooooo");
+    }
+    // sobrescrita de método
+    @Override
+    public void ataqueBase(){
+        System.out.println("Eu vou tacar uma kinai do elemento fogo em você, toma!");
+    }
+
+     public void ataqueBase(int nivelDeChacra){
+        if(nivelDeChacra > 20){
+            System.out.println("Susado ativado!!");
+        }else if (nivelDeChacra < 1){
+            System.out.println("Eu só consegui ativar isso aí, brow, o sharingan");
+        }else{
+            System.out.println("Eu to sem chacra.");
+        }
     }
 }
